653de00ccb326945f69b3e184224a88288075dce
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-05-12 23:12:34 -0500

    background-image added for all buttons

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//
+//If you put handleClick(), it will straight up call the function without checking click.
+//So we need to use handleClick so that it passes the name of the function handleClick.
+//This makes the function wait till click is triggered.
+//document.querySelector("button").addEventListener("click", handleClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
+    });
+
+}
+
+// var audio = new Audio("/sounds/tom-1.mp3"); //creating HTML audio Element
+//     audio.play();
