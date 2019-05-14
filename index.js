1ed9f56c9261835c20c13453bc823008399fd91e
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-05-13 22:55:05 -0500

    event listner for keyboard keys added

--- index.js
+++ index.js
@@ -8,45 +8,56 @@ for (var i=0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     
         var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
 
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio("/sounds/tom-1.mp3"); //creating HTML audio Element
-                tom1.play();
-                break;
-
-            case "a":
-                var tom2 = new Audio("/sounds/tom-2.mp3"); //creating HTML audio Element
-                tom2.play();
-                break;
-
-            case "s":
-                var tom3 = new Audio("/sounds/tom-3.mp3"); //creating HTML audio Element
-                tom3.play();
-                break;
-
-            case "d":
-                var tom4 = new Audio("/sounds/tom-4.mp3"); //creating HTML audio Element
-                tom4.play();
-                break;
-
-            case "j":
-                var snare = new Audio("/sounds/snare.mp3"); //creating HTML audio Element
-                snare.play();
-                break;
-
-            case "k":
-                var crash = new Audio("/sounds/crash.mp3"); //creating HTML audio Element
-                crash.play();
-                break;
-
-            case "l":
-                var kick = new Audio("/sounds/kick-bass.mp3"); //creating HTML audio Element
-                kick.play();
-                break;
-
-            default: console.log(buttonInnerHTML);
-
-            }
     });
+}
+
+//function(event) -- can be any name funtion(apple)--gives same result.-- i.e. the event.
+document.addEventListener("keypress", function(event) {     //Attach event listner to whole webpage
+
+        makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+
+        case "w":
+            var tom1 = new Audio("/sounds/tom-1.mp3"); //creating HTML audio Element
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("/sounds/tom-2.mp3"); //creating HTML audio Element
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("/sounds/tom-3.mp3"); //creating HTML audio Element
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("/sounds/tom-4.mp3"); //creating HTML audio Element
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("/sounds/snare.mp3"); //creating HTML audio Element
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio("/sounds/crash.mp3"); //creating HTML audio Element
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio("/sounds/kick-bass.mp3"); //creating HTML audio Element
+            kick.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+
+        }
 }
