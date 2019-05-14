99ee1aaa8605412372aa57ac75f1e1a5d7810293
Subodh Dhakal <subodh.dhakal@mavs.uta.edu>
2019-05-13 22:21:08 -0500

    sounds added in switch statement

--- index.js
+++ index.js
@@ -5,13 +5,48 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i < numberOfDrumButtons; i++) {
-
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     
+        var buttonInnerHTML = this.innerHTML;
 
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("/sounds/tom-1.mp3"); //creating HTML audio Element
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("/sounds/tom-2.mp3"); //creating HTML audio Element
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("/sounds/tom-3.mp3"); //creating HTML audio Element
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("/sounds/tom-4.mp3"); //creating HTML audio Element
+                tom4.play();
+                break;
+
+            case "j":
+                var snare = new Audio("/sounds/snare.mp3"); //creating HTML audio Element
+                snare.play();
+                break;
+
+            case "k":
+                var crash = new Audio("/sounds/crash.mp3"); //creating HTML audio Element
+                crash.play();
+                break;
+
+            case "l":
+                var kick = new Audio("/sounds/kick-bass.mp3"); //creating HTML audio Element
+                kick.play();
+                break;
+
+            default: console.log(buttonInnerHTML);
+
+            }
     });
-
 }
-
-// var audio = new Audio("/sounds/tom-1.mp3"); //creating HTML audio Element
-//     audio.play();
